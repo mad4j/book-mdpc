d263d917e6003f182ed54e414dd97d529c45057f
Daniele Olmisani <daniele.olmisani@gmail.com>
2016-08-21 15:52:24 +0200

    fixed build script

--- deploy.sh
+++ deploy.sh
@@ -1,10 +1,21 @@
 #!/bin/bash
-rm -fR dist/
-mkdir -p dist/book-mdpc/examples
-cp -R examples/ dist/book-mdpc/examples
-cp examples.properties dist/book-mdpc
-cp examples.properties dist/book-mdpc.txt
-cp LICENSE dist/book-mdpc
-cp README.md dist/book-mdpc
-zip -rq dist/book-mdpc.zip dist/book-mdpc
-rm -fR dist/book-mdpc/
+
+TARGET=doc
+BUILD=`git rev-list HEAD | wc -l`
+VERSION=`git describe --tags`
+
+echo "Building '$VERSION' / build '$BUILD' in folder '$TARGET'..."
+
+rm -fR $TARGET/
+mkdir -p $TARGET/book-mdpc/examples
+cp -R examples/ $TARGET/book-mdpc/examples
+sed -e "s/##BUILD##/$BUILD/g" -e "s/##VERSION##/$VERSION/g" examples.properties.template  > examples.properties
+cp examples.properties $TARGET/book-mdpc
+cp examples.properties $TARGET/book-mdpc.txt
+cp LICENSE $TARGET/book-mdpc
+cp README.md $TARGET/book-mdpc
+zip -rq $TARGET/book-mdpc.zip $TARGET/book-mdpc
+rm examples.properties
+rm -fR $TARGET/book-mdpc/
+
+echo "...DONE"
